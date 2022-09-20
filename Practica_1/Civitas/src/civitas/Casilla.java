f1c69a731f78e58fdd0e47677b2409a07fa6a799
Antonio <antoniorr@correo.ugr.es>
2022-09-20 12:08:57 +0200

    casilla

---
+++ Practica_1/Civitas/src/civitas/Casilla.java
@@ -0,0 +1,58 @@
+package civitas;
+
+/**
+ *
+ * @author antonio
+ */
+public class Casilla {
+    private TipoCasilla tipo;
+    private String nombre;
+    private float precioCompra, precioEdificar, precioBaseAlquiler;
+    private int numCasas, numHoteles;
+    
+    public Parcela(TipoCasilla type, String name, float preciocompra, float precioedificar, float preciobasealquiler) {
+        this.tipo = type;
+        this.nombre = name;
+        this.precioCompra = preciocompra;
+        this.precioEdificar = precioedificar;
+        this.precioBaseAlquiler = preciobasealquiler;
+        this.numCasas = 0;
+        this.numHoteles = 0;        
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public float getPrecioCompra(){
+        return precioCompra;
+    }
+    
+    public float getPrecioEdificar(){
+        return precioEdificar;
+    }
+    
+    public int getNumCasas() {
+        return numCasas;
+    }
+    
+    public int getNumHoteles() {
+        return numHoteles;
+    }
+    
+    public float getPrecioAlquilerCompleto() {
+        return precioBaseAlquiler * (1 + numCasas + numHoteles*4);
+    }
+    
+    public boolean construirCasa() {
+        int inicio = numCasas;
+        numCasas++;
+        return inicio == numCasas-1; // Comprobamos si el número de casas ha aumentado en 1.
+    }
+    
+    public boolean construirHotel() {
+        int inicio = numHoteles;
+        numHoteles++;
+        return inicio == numHoteles-1; // Comprobamos si el número de casas ha aumentado en 1.
+    }
+}
