f302ac9841f4de553e101033a0204c93fbae1afb
Pepe <e.jacarmona@go.ugr.es>
2022-09-20 11:31:54 +0200

    jola

--- Practica_1/Civitas/src/civitas/TipoCasilla.java
+++ Practica_1/Civitas/src/civitas/TipoCasilla.java
@@ -1,1 +1,11 @@
 public enum TipoCasilla {CALLE, SORPRESA, DESCANSO};
+
+String Nombre = new String(nombrecasilla);
+
+float precioCompra[]=new float[20];
+float precioEdificar[]=new float[20];
+float precioBaseAlquiler[]=new float[20];
+
+int numCasas[]=new int;
+int numHoteles[]=new int;
+
