8bf0a5a221a8c5704e4a9eddf73340a3ccc55abd
Antonio <antoniorr@correo.ugr.es>
2022-09-20 23:54:00 +0200

    Comprobar implementacion de tablero.java

---
+++ Practica_1/Civitas/src/civitas/Tablero.java
@@ -0,0 +1,47 @@
+package civitas;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author antonio
+ */
+public class Tablero {
+    private ArrayList<Casilla> casillas;
+    private boolean porSalida;
+        
+    protected Tablero(){
+        casillas = null;
+        porSalida = false;
+    }
+    
+    private boolean correcto(int numCasilla) {
+        return numCasilla < casillas.size();
+    }
+    
+    protected boolean computarPasoPorSalida() {
+        boolean inicial = porSalida;
+        porSalida = false;
+        return inicial;
+    }
+    
+    protected void añadeCasilla(Casilla casilla) {
+        casillas.add(casilla);
+    }
+    
+    protected Casilla getCasilla(int numCasilla) {
+        if (correcto(numCasilla))
+            return casillas.get(numCasilla);
+        else
+            return null;
+    }
+    
+    protected int nuevaPosicion(int actual, int tirada) {
+        int pos = actual + tirada;
+        if (pos >= casillas.size()) {
+            porSalida = true;
+            pos %= casillas.size();
+        }
+        return pos;
+    }    
+}
