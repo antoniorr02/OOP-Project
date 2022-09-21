8220d6ee5ffddf6b1b5e2ae308d5b44fa290c622
Antonio <antoniorr@correo.ugr.es>
2022-09-21 12:44:29 +0200

    MI MAZO

--- Practica_1/Civitas/src/civitas/MazoSorpresa.java
+++ Practica_1/Civitas/src/civitas/MazoSorpresa.java
@@ -1,5 +1,7 @@
 package civitas;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author antonio
@@ -14,5 +16,31 @@ public class MazoSorpresa {
         sorpresas = null;
         barajada = false;
         usadas = 0;
+    }
+
+    protected MazoSorpresa() {
+        init();
+        debug = false;
+    }
+
+    protected MazoSorpresa(boolean d) {
+        debug = d;
+        init();
+        if (debug) {}
+            // ver como se informa en diario.
+    }
+
+    protected void alMazo(Sorpresa s) {
+        if(!barajada)
+            sorpresas.add(s);
+    }
+
+    protected Sorpresa siguiente() {
+        if ((!barajada || usadas == sorpresas.size()) && !debug) {
+            // BARAJAR?
+            usadas = 0;
+            barajada = true;
+        }
+
     }
 }
