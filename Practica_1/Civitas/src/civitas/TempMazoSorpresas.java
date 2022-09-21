42e73547f8c9d29760944bbd40cd319a5f29b857
Pepe <e.jacarmona@go.ugr.es>
2022-09-21 12:33:10 +0200

    Clase MazoSorpresa temporal

---
+++ Practica_1/Civitas/src/civitas/TempMazoSorpresas.java
@@ -0,0 +1,33 @@
+package civitas;
+
+public class TempMazoSorpresas {
+    
+    private ArrayList<Sorpresa> sorpresas;
+    private  boolean barajada;
+    private int usadas;
+    private boolean debug;
+
+    private void init(){
+        sorpresas = null;
+        barajada = false;
+        usadas = 0;
+    }
+    
+    protected TempMazoSorpresas(debug){ //comprobar lo del diario
+        if(debug)
+            return init();
+        }
+    }
+
+    protected TempMazoSorpresas(){
+        return debug = false && init();
+    }
+
+    void alMazo(Sorpresa s){
+
+    }
+
+    Sorpresa siguiente(){
+
+    }
+}
