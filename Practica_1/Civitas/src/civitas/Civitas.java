19a593468a7b61f4d1ebc071f98e71aa72c1d443
Pepe <e.jacarmona@go.ugr.es>
2022-09-22 12:49:44 +0200

    Ej4 main

--- Practica_1/Civitas/src/civitas/Civitas.java
+++ Practica_1/Civitas/src/civitas/Civitas.java
@@ -18,7 +18,7 @@ public class Civitas {
         int n1 = 0;
         int n2 = 0;
         int n3 = 0;
-        for (int i =0 ; i <= 100; i++) {
+        for (int i = 0 ; i <= 100; i++) {
             int n = Dado.getInstance().quienEmpieza(4);
             switch(n){
                 case 0: n0++;
@@ -44,6 +44,49 @@ public class Civitas {
         System.out.println("getUltimoResultado: " + Dado.getInstance().getUltimoResultado());
 
         // No entiendo el enunciado del apartado 4 del main.
+        // El plan es tirar unas 50 veces e imprimir por pantalla un ejemplo de cada valor, es decir,
+        // que saldrían 6 mensajes de "en la tirada i(1-50) ha salido el valor n (1-6)", ya que solo se imprimen la 
+        // primera vez
+
+        for(int i = 0; i < 50; i++){
+            int uno = 0;
+            int dos = 0;
+            int tres = 0;
+            int cuatro = 0;
+            int cinco = 0;
+            int seis = 0;
+
+            Dado.getInstance().setDebug(true);
+            Dado.getInstance().tirar();
+            int n = Dado.getInstance().getUltimoResultado();
+            switch(n){
+                case 1: if (uno == 0)
+                            System.out.println("En la tirada " + i + " ha salido un " + n);
+                            uno = 1;
+                    break;
+                case 2: if (dos == 0)
+                            System.out.println("En la tirada " + i + " ha salido un " + n);
+                            dos = 1;
+                    break;
+                case 3: if (tres == 0)
+                            System.out.println("En la tirada " + i + " ha salido un " + n);
+                            tres = 1;
+                    break;
+                case 4: if (cuatro == 0)
+                            System.out.println("En la tirada " + i + " ha salido un " + n);
+                            cuatro = 1;
+                    break;
+                case 5: if (cinco == 0)
+                            System.out.println("En la tirada " + i + " ha salido un " + n);
+                            cinco = 1;
+                    break;
+                case 6: if (seis == 0)
+                            System.out.println("En la tirada " + i + " ha salido un " + n);
+                            seis = 1;
+        
+                }
+
+        }
 
         Tablero tablero = new Tablero();
         Casilla nueva1 = new Casilla(TipoCasilla.CALLE,"Reyes Católicos", 10, 20,5);
