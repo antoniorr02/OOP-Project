ad1dcb66e0928d59c0b90fbd9248c63816bd6fe5
Pepe <e.jacarmona@go.ugr.es>
2022-09-29 12:39:30 +0200

    Parcela2 Instancias de clase

--- EjerciciosJava/src/ejerciciosjava/Parcela2_ruby.rb
+++ EjerciciosJava/src/ejerciciosjava/Parcela2_ruby.rb
@@ -1,31 +1,28 @@
 # encoding:utf-8
 
 class Parcela
-    attr_accessor :nombre, :precioCompra, :precioEdificar, :precioBaseAlquiler, :numCasas, :numHoteles
 
-    def initialize(nombre, precioCompra, precioEdificar, precioBaseAlquiler)
-        @nombre = nombre
-        @precioCompra = precioCompra
-        @precioEdificar = precioEdificar
-        @precioBaseAlquiler = precioBaseAlquiler
-        @numCasas = 0
-        @numHoteles = 0
-    end
-
-    def construir_casa
+    @nombre = nombre
+    @precioCompra = precioCompra
+    @precioEdificar = precioEdificar
+    @precioBaseAlquiler = precioBaseAlquiler
+    @numCasas = 0
+    @numHoteles = 0
+    
+    def self.construir_casa
         @numCasas += 1
     end
 
-    def construir_hotel
+    def self.construir_hotel
         @numHoteles += 1
     end
 
-    def getPrecioAlquilerCompleto
+    def self.getPrecioAlquilerCompleto
         puts @precioBaseAlquiler * (1 + @numCasas*1 + @numHoteles*4)
     end
 end
 
-parcela = Parcela.new("nombre", 10, 20,5)
+parcela = Parcela.new
 parcela.construir_casa()
 parcela.construir_hotel()
 parcela.getPrecioAlquilerCompleto()
