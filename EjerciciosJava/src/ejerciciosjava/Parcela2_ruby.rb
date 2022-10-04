97a8df24894ce2bd9e4b2fe2c84ae06ff5c0c56c
Antonio <antoniorr@correo.ugr.es>
2022-10-04 10:47:06 +0200

    Ver xq no funcionan setter en var. instancia clase

--- EjerciciosJava/src/ejerciciosjava/Parcela2_ruby.rb
+++ EjerciciosJava/src/ejerciciosjava/Parcela2_ruby.rb
@@ -2,12 +2,57 @@
 
 class Parcela
 
-    @nombre = nombre
-    @precioCompra = precioCompra
-    @precioEdificar = precioEdificar
-    @precioBaseAlquiler = precioBaseAlquiler
+    @nombre
+    @precioCompra
+    @precioEdificar
+    @precioBaseAlquiler = 0
     @numCasas = 0
     @numHoteles = 0
+
+#Getters:
+    def self.nombre
+        @nombre
+    end
+
+    def self.precioCompra
+        @precioCompra
+    end
+
+    def self.precioEdificar
+        @precioEdificar
+    end
+
+    def self.precioBaseAlquiler
+        @precioBaseAlquiler
+    end
+
+    def self.numCasas
+        @numCasas
+    end
+
+    def numHoteles
+        @numHoteles
+    end
+
+#Setters:
+
+    def self.set_nombre(nombre)
+        @nombre = nombre
+    end
+
+    def self.set_precioCompra(precioCompra)
+        @precioCompra = precioCompra
+    end
+
+    def self.set_precioEdificar(precioEdificar)
+        @precioEdificar = precioEdificar
+    end
+
+    def self.set_precioBaseAlquiler(precioBaseAlquiler)
+        @nombre = precioBaseAlquiler
+    end
+
+#Methods:
     
     def self.construir_casa
         @numCasas += 1
@@ -22,7 +67,8 @@ class Parcela
     end
 end
 
-parcela = Parcela.new
-parcela.construir_casa()
-parcela.construir_hotel()
-parcela.getPrecioAlquilerCompleto()
+Parcela.construir_casa
+Parcela.construir_hotel
+Parcela.set_precioBaseAlquiler(10)
+puts Parcela.precioBaseAlquiler
+Parcela.getPrecioAlquilerCompleto
