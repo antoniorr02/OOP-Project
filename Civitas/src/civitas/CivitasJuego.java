d17516f79fdc8aef5e39d53e6842622f55bfe15d
Antonio <antoniorr@correo.ugr.es>
2022-11-02 17:11:52 +0100

    casillas juanma

--- Civitas/src/civitas/CivitasJuego.java
+++ Civitas/src/civitas/CivitasJuego.java
@@ -45,11 +45,39 @@ public class CivitasJuego {
 
     private void inicializaTablero(MazoSorpresa m) {
         tablero = new Tablero();
-    } // No sé que más poner
+        tablero.añadeCasilla(new Casilla("Avenida Europa", 2500, 500, 200));
+        tablero.añadeCasilla(new Casilla("Calle Nueva", 2000, 450, 180));
+        tablero.añadeCasilla(new Casilla("Casilla Sorpresa", mazo));
+        tablero.añadeCasilla(new Casilla("Calle Perdida", 1500, 250, 160));
+        tablero.añadeCasilla(new Casilla("Plaza España", 2300, 500, 250));
+        tablero.añadeCasilla(new Casilla("Paseo de los Pibes", 4000, 530, 320));
+        tablero.añadeCasilla(new Casilla("Casilla Sorpresa", mazo));
+        tablero.añadeCasilla(new Casilla("Calle de la Fuente", 2800, 400, 230));
+        tablero.añadeCasilla(new Casilla("Campus de la Cartuja", 3300, 470, 290));
+        tablero.añadeCasilla(new Casilla("Parking"));
+        tablero.añadeCasilla(new Casilla("Avenida Victoria", 2700, 350, 200));
+        tablero.añadeCasilla(new Casilla("Gran Vía", 4500, 620, 350));
+        tablero.añadeCasilla(new Casilla("Casilla Sorpresa", mazo));
+        tablero.añadeCasilla(new Casilla("Calle Estudiantes", 1300, 200, 160));
+        tablero.añadeCasilla(new Casilla("Cibeles", 3000, 600, 300));
+        tablero.añadeCasilla(new Casilla("Avenida Granada", 2800, 520, 280));
+        tablero.añadeCasilla(new Casilla("Casilla Sorpresa", mazo));
+        tablero.añadeCasilla(new Casilla("Travesía de Locos", 2900, 390, 240));
+        tablero.añadeCasilla(new Casilla("Carretera de Jaén", 1900, 300, 230));
+    }
 
     private void inicializaMazoSorpresas() {
-
-    } // Cuales son las cartas??
+        mazo.alMazo(new Sorpresa(TipoSorpresa.PAGARCOBRAR,"Rompes una farola de una pedrada, pagas 100€",-100));
+        mazo.alMazo(new Sorpresa(TipoSorpresa.PAGARCOBRAR,"Te rompes la rodilla y pagas al médico 200€",-200));
+        mazo.alMazo(new Sorpresa(TipoSorpresa.PAGARCOBRAR,"Robas una joyeria y te pilla la policía, pagas 300€",-300));
+        mazo.alMazo(new Sorpresa(TipoSorpresa.PAGARCOBRAR,"Ganas concurso de belleza, recibes 100€",100));
+        mazo.alMazo(new Sorpresa(TipoSorpresa.PAGARCOBRAR,"Ganas la quiniela, recibes 200€",200));
+        mazo.alMazo(new Sorpresa(TipoSorpresa.PAGARCOBRAR,"Eres un potroso, encuentras un billete de 500€ en la acera",500));
+        mazo.alMazo(new Sorpresa(TipoSorpresa.PORCASAHOTEL,"Haces reformas en todas tus viviendas, pagas 200€ por cada propieda",-200));
+        mazo.alMazo(new Sorpresa(TipoSorpresa.PORCASAHOTEL,"Viene un huracán y gastas en arreglos 500€ por vivienda",-500));
+        mazo.alMazo(new Sorpresa(TipoSorpresa.PORCASAHOTEL,"Pedro Sanchez da ayudas de 200€ a cada vivienda",200));
+        mazo.alMazo(new Sorpresa(TipoSorpresa.PORCASAHOTEL,"Vendes todos tus muebles de cada vivienda, ganas 500€ por vivienda",500));
+    }
     
     public Jugador getJugadorActual() {
         return jugadores.get(indiceJugadorActual);
